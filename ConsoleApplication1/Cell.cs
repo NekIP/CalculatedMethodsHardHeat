8f24fe79510b74290c67fb95462d723750c235a3
Nikita <Shestakov.rm@list.ru>
2018-04-16 17:22:11 +0300

    First commit

---
+++ ConsoleApplication1/Cell.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApplication1 {
+    public class Cell {
+        public int Id { get; set; }
+        public double S { get; set; }
+        public double SOld { get; set; }
+        public Cell(double s) {
+            S = s;
+            SOld = s;
+        }
+    }
+}
---
+++ ConsoleApplication1/Edge.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApplication1 {
+    public class Edge {
+        public int Id { get; set; }
+        public Node Node1 { get; set; }
+        public Node Node2 { get; set; }
+        public TriangleCell Cell1 { get; set; }
+        public TriangleCell Cell2 { get; set; }
+        public Vector Normal { get; set; }
+        public double L { get; set; }
+        public int CellsCount { get; set; }
+        public List<Point> C { get; set; }
+        public int Type { get; set; }
+    }
+}
